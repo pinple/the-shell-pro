a35a832043f9e1b2f9a50c8ec50f8a5a201ec504
Neulana <fupinyou@gmail.com>
2018-12-25 10:07:15 +0800

    🐛 fix gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
+'use strict';
+ 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-
-gulp.task('scss',function(){
-    gulp.src('./assets/scss/*.scss')  //这里是scss文件的目录
-        .pipe(sass().on('error',sass.logError))
-        .pipe(gulp.dest('./assets/css'));  //这里是编译后css存放的目录
-})
-
-gulp.task('default',function(){
-    gulp.watch('./assets/scss/*.scss',['scss']);  //在这里执行文件观察任务，发现变化执行上面定义好的 `scss`编译任务。
-})
+ 
+sass.compiler = require('node-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./assets/scss/screen.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./assets/css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./assets/scss/screen.scss', gulp.series('sass'));
+});
