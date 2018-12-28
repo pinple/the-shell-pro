cdf2aa05581583f67a1eaef965df1d6ef006d56f
Neulana <fupinyou@gmail.com>
2018-12-28 22:03:29 +0800

    ✨ adjust font-size and logo margin

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,11 @@ var sass = require('gulp-sass');
 sass.compiler = require('node-sass');
  
 gulp.task('sass', function () {
-  return gulp.src('./assets/scss/screen.scss')
+  return gulp.src('./assets/scss/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./assets/css'));
 });
  
 gulp.task('sass:watch', function () {
-  gulp.watch('./assets/scss/screen.scss', gulp.series('sass'));
+  gulp.watch('./assets/scss/*.scss', gulp.series('sass'));
 });
